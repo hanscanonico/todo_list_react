3e21e053ac18970eee1d1eba339ae23af0e7545e
Hans Canonico <hanscanonico@air-de-hans.home>
2024-05-01 21:26:55 +0200

    fix URI redirection

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -1,5 +1,6 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL
 
 export const redirectToGoogleOAuth = async () => {
-    window.location.href = `${BASE_URL}/auth/google_oauth2?redirect_uri=http://localhost:3030/login`
+    window.location.href = `${BASE_URL}/auth/google_oauth2?redirect_uri=${REDIRECT_URI}/login`
 }
